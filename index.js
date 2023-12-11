a3fe1e47bc3da1aa0dd800cbcbfa5659624b42bb
Quy <quy.pham@stunited.vn>
2023-12-11 16:56:32 +0700

    add position and skill db

--- index.js
+++ index.js
@@ -132,6 +132,178 @@ app.patch("/tracking/:id", (req, res) => {
   })();
 });
 
+//Position
+
+app.post("/position", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("position");
+      const response = [];
+      await query
+        .get()
+        .then((querySnapshot) => {
+          const docs = querySnapshot.docs; // the result of our query
+          for (const doc of docs) {
+            // add each doc to our JSON response
+            const selectedItem = {
+              id: doc.id,
+              ...doc.data(),
+            };
+            response.push(selectedItem);
+          }
+        })
+        .then(async (respro) => {
+          const id = response.length + 1;
+          await db
+            .collection("position")
+            .doc("/" + id + "/")
+            .create({ ...req.body, createdAt: new Date().toISOString() });
+          return res.status(200).send({msg: 'Success', data: { ...req.body, createdAt: new Date().toISOString() }});
+        });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.get("/position/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("position").doc(req.params.id);
+      const user = await document.get();
+      const response = user.data();
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.get("/position", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("position");
+      const response = [];
+      await query.get().then((querySnapshot) => {
+        const docs = querySnapshot.docs; // the result of our query
+        for (const doc of docs) {
+          // add each doc to our JSON response
+          const selectedItem = {
+            id: doc.id,
+            ...doc.data(),
+          };
+          response.push(selectedItem);
+        }
+        return response; // each then should return a value
+      });
+      return res.status(200).send(response); // end of async function should return a value
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.patch("/position/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("position").doc(req.params.id);
+      await document.update(req.body);
+      return res.status(200).send(req.body);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.post("/skill", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("skill");
+      const response = [];
+      await query
+        .get()
+        .then((querySnapshot) => {
+          const docs = querySnapshot.docs; // the result of our query
+          for (const doc of docs) {
+            // add each doc to our JSON response
+            const selectedItem = {
+              id: doc.id,
+              ...doc.data(),
+            };
+            response.push(selectedItem);
+          }
+        })
+        .then(async (respro) => {
+          const id = response.length + 1;
+          await db
+            .collection("skill")
+            .doc("/" + id + "/")
+            .create({ ...req.body, createdAt: new Date().toISOString() });
+          return res.status(200).send({msg: 'Success', data: { ...req.body, createdAt: new Date().toISOString() }});
+        });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.get("/skill/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("skill").doc(req.params.id);
+      const user = await document.get();
+      const response = user.data();
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.get("/skill", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("skill");
+      const response = [];
+      await query.get().then((querySnapshot) => {
+        const docs = querySnapshot.docs; // the result of our query
+        for (const doc of docs) {
+          // add each doc to our JSON response
+          const selectedItem = {
+            id: doc.id,
+            ...doc.data(),
+          };
+          response.push(selectedItem);
+        }
+        return response; // each then should return a value
+      });
+      return res.status(200).send(response); // end of async function should return a value
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.patch("/skill/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("skill").doc(req.params.id);
+      await document.update(req.body);
+      return res.status(200).send(req.body);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
 // Create
 app.post("/employees", (req, res) => {
   (async () => {
