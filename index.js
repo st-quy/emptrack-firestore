71dc78e070d38b814c6d00b62c3b5813e2beca2e
Quy <quy.pham@stunited.vn>
2023-12-14 15:59:39 +0700

    technology

--- index.js
+++ index.js
@@ -47,6 +47,91 @@ app.post("/login", (req, res) => {
 } 
 });
 
+app.post("/technology", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("technology");
+      const response = [];
+      await query
+        .get()
+        .then((querySnapshot) => {
+          const docs = querySnapshot.docs; // the result of our query
+          for (const doc of docs) {
+            // add each doc to our JSON response
+            const selectedItem = {
+              id: doc.id,
+              ...doc.data(),
+            };
+            response.push(selectedItem);
+          }
+        })
+        .then(async (respro) => {
+          const id = response.length + 1;
+          await db
+            .collection("technology")
+            .doc("/" + id + "/")
+            .create({ ...req.body, createdAt: new Date().toISOString() });
+          return res.status(200).send({msg: 'Success', data: { ...req.body, createdAt: new Date().toISOString() }});
+        });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.get("/technology/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("technology").doc(req.params.id);
+      const user = await document.get();
+      const response = user.data();
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.get("/technology", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("technology");
+      const response = [];
+      await query.get().then((querySnapshot) => {
+        const docs = querySnapshot.docs; // the result of our query
+        for (const doc of docs) {
+          // add each doc to our JSON response
+          const selectedItem = {
+            id: doc.id,
+            ...doc.data(),
+          };
+          response.push(selectedItem);
+        }
+        return response; // each then should return a value
+      });
+      return res.status(200).send(response); // end of async function should return a value
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.patch("/technology/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("technology").doc(req.params.id);
+      await document.update(req.body);
+      return res.status(200).send(req.body);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
 app.post("/tracking", (req, res) => {
   (async () => {
     try {
